c43365f650c8c96437982a49037525085a71f901
Ahmed Sabbour <asabbour@microsoft.com>
2018-03-26 17:04:53 +0200

    Attempt to fix formatting

--- proctor/yaml/grafana-prometheus/deploy.sh
+++
@@ -1,2 +0,0 @@
-helm install --name prometheus-k8schallenge stable/prometheus -f prometheus-configforhelm.yaml --namespace k8schallenge-monitoring
-helm install --name graphana-k8schallenge stable/grafana --set server.service.type=LoadBalancer,server.adminUser=admin,server.adminPassword=admin  --namespace k8schallenge-monitoring
