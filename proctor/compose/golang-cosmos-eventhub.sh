7dac884b6c6a09603baa289498bc28a9c80f279e
Ahmed Sabbour <103856+sabbour@users.noreply.github.com>
2018-11-05 13:08:36 +0400

    Switched to packaging site as a container

--- proctor/compose/golang-cosmos-eventhub.sh
+++
@@ -1,5 +0,0 @@
-pushd ./cosmos-eventhub/golang
-docker-compose build
-docker-compose push
-docker-compose up
-popd
--- proctor/compose/netcore-cosmos-eventhub.sh
+++
@@ -1,5 +0,0 @@
-pushd ./cosmos-eventhub/netcore
-docker-compose build
-docker-compose push
-docker-compose up
-popd
