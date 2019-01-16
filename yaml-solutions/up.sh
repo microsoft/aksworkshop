28d64d7a24e97994d9c58250d4a6b7a210558d8c
Ahmed Sabbour <asabbour@microsoft.com>
2019-01-15 16:53:15 -0800

    Uncommented lines in up.sh

--- yaml-solutions/up.sh
+++ yaml-solutions/up.sh
@@ -5,35 +5,35 @@ export K8SVERSION=1.11.5
 export DURATION=1m
 export CONCURRENT=1000
 
-# echo "Logging in to Azure CLI"
-# az login
+echo "Logging in to Azure CLI"
+az login
 
-# echo "\nCreating Resource Group"
-# az group create --name $RGNAME --location $LOCATION
+echo "\nCreating Resource Group"
+az group create --name $RGNAME --location $LOCATION
 
-# echo "\nCreating AKS cluster"
-# az aks create --resource-group $RGNAME --name $AKSNAME --enable-addons monitoring --kubernetes-version $K8SVERSION --generate-ssh-keys --location $LOCATION
+echo "\nCreating AKS cluster"
+az aks create --resource-group $RGNAME --name $AKSNAME --enable-addons monitoring --kubernetes-version $K8SVERSION --generate-ssh-keys --location $LOCATION
 
-# echo "\nSleeping for 10 seconds to wait for cluster to stabilize"
-# sleep 10
+echo "\nSleeping for 10 seconds to wait for cluster to stabilize"
+sleep 10
 
-# echo "\nRetrieving cluster credentials"
-# az aks get-credentials --resource-group $RGNAME --name $AKSNAME --overwrite-existing
+echo "\nRetrieving cluster credentials"
+az aks get-credentials --resource-group $RGNAME --name $AKSNAME --overwrite-existing
 
-# echo "\nInstalling kubectl"
-# az aks install-cli
+echo "\nInstalling kubectl"
+az aks install-cli
 
-# echo "\nRetrieving nodes"
-# kubectl get nodes
+echo "\nRetrieving nodes"
+kubectl get nodes
 
-# echo "\nSetting up Tiller Service Account"
-# kubectl apply -f http://aksworkshop.io/yaml-solutions/01.%20challenge-02/helm-rbac.yaml
+echo "\nSetting up Tiller Service Account"
+kubectl apply -f http://aksworkshop.io/yaml-solutions/01.%20challenge-02/helm-rbac.yaml
 
-# echo "\nSetting up Log Reader Service Account"
-# kubectl apply -f http://aksworkshop.io/yaml-solutions/01.%20challenge-03/logreader-rbac.yaml
+echo "\nSetting up Log Reader Service Account"
+kubectl apply -f http://aksworkshop.io/yaml-solutions/01.%20challenge-03/logreader-rbac.yaml
 
-# echo "\nInitializing Helm"
-# helm init --service-account tiller
+echo "\nInitializing Helm"
+helm init --service-account tiller
 
 echo "\nWaiting for Tiller pod to be Running"
 TILLERSTATUS=""
